7aa33b7b5a7bb965196304794463abe04343a71a
Darkbuilder646 <theolejeune.tl@gmail.com>
2023-10-24 16:03:20 +0200

    Init Tailwind

---
+++ ShaderWeb/src/Pages/ShaderPage.jsx
@@ -0,0 +1,9 @@
+const ShaderPage = () => {
+    return(
+        <div className="flex justify-center items-center">
+            <h1 className=" text-center text-cyan-200 font-bold">ShaderWeb</h1>
+        </div>
+    )
+}
+
+export default ShaderPage;
--- ShaderWeb/src/main.jsx
+++ ShaderWeb/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import ShaderPage from './Pages/ShaderPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ShaderPage />
   </React.StrictMode>,
 )
